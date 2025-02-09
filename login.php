59706c8e255ad99885d8834d500edca2229f611a
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2025-02-09 13:49:13 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "root123";
+$database = "lab_db";
+
+// Conexión a la base de datos
+$conn = new mysqli($servername, $username, $password, $database);
+
+$user = $_POST['username'];
+$pass = $_POST['password'];
+
+$query = "SELECT password FROM users WHERE username = '$user' AND password = '$pass'";
+
+$result = $conn->query($query);
+if (!$result) {
+    die("Error en la consulta SQL: " . $conn->error);
+}
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo htmlentities($row['password']) . "<br>";
+    }
+    echo "PR!M€R@_FlÆG/%0";
+} else {
+    echo "Usuario o contraseña incorrectos.";
+}
+
+$conn->close();
+?>
